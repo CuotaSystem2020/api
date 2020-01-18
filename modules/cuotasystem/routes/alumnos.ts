1102e549221888e70330cb5c76acd57fe67a57b7
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-01-18 02:24:05 -0300

    Se corrige bug en edición de alumno

--- modules/cuotasystem/routes/alumnos.ts
+++ modules/cuotasystem/routes/alumnos.ts
@@ -1,60 +1,58 @@
-import * as express from "express";
-import * as mongoose from "mongoose";
-import { alumnos } from "./../schemas/alumnos";
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import { alumnos } from './../schemas/alumnos';
 
 var router = express.Router();
 
-router.get("/alumnos", function(req, res, next) {
-  let activo: any = {};
+router.get('/alumnos', function(req, res, next) {
+	let activo: any = {};
 
-  if (req.query.activo === "true") {
-    activo["activo"] = true;
-  }
+	if (req.query.activo === 'true') {
+		activo['activo'] = true;
+	}
 
-  let query = alumnos.find(activo).sort({ apellido: 1 });
+	let query = alumnos.find(activo).sort({ apellido: 1 });
 
-  query.exec(function(err, data) {
-    if (err) {
-      return next(err);
-    }
-    // TODO: refactorizar todo a esta forma res.status(200).json(data);
-    res.status(200).json(data);
-  });
+	query.exec(function(err, data) {
+		if (err) {
+			return next(err);
+		}
+		// TODO: refactorizar todo a esta forma res.status(200).json(data);
+		res.status(200).json(data);
+	});
 });
 
-router.get("/alumnoId/:id", function(req, res, next) {
-  let query = alumnos.findById(req.params.id);
+router.get('/alumnos/:id', (req, res, next) => {
+  console.log("Reqqq: ", req.params.id);
+	let query = alumnos.findById(req.params.id);
 
-  query.exec(function(err, data) {
-    if (err) {
-      return next(err);
-    }
-    res.status(200).json(data);
-  });
+	query.exec(function(err, data) {
+		if (err) {
+			return next(err);
+		}
+		res.status(200).json(data);
+	});
 });
 
-router.post("/alumno", function(req: any, res, next) {
-  let alumno = new alumnos(req.body);
-  
-  alumno.save(function(err, alumnoCreado) {
-    if (err) {
-      res.status(500).send(err);
-    }
-    
-    res.status(200).json(alumnoCreado);
-  });
+router.post('/alumno', function(req: any, res, next) {
+	let alumno = new alumnos(req.body);
+
+	alumno.save(function(err, alumno) {
+		if (err) {
+			res.status(500).send(err);
+		}
+		console.log('Alumno: ', alumno);
+		res.json(alumno);
+	});
 });
 
-router.put("/alumno/:_id", function(req, res, next) {
-  alumnos.findByIdAndUpdate(req.params._id, req.body, { new: true }, function(
-    err,
-    data
-  ) {
-    if (err) {
-      return next(err);
-    }
-    res.json(data);
-  });
+router.put('/alumno/:_id', function(req, res, next) {
+	alumnos.findByIdAndUpdate(req.params._id, req.body, { upsert: true }, function(err, alumno) {
+		if (err) {
+			return next(err);
+		}
+		res.status(200).json(alumno);
+	});
 });
 
 export = router;
--- modules/cuotasystem/schemas/alumnos.ts
+++ modules/cuotasystem/schemas/alumnos.ts
@@ -1,37 +1,40 @@
-import * as mongoose from "mongoose";
-import * as constantes from "../../../utils/constantes";
-import * as escuelaSchema from "../../tm/schemas/escuela";
-import * as autorizadosSchema from "../../tm/schemas/autorizados";
-import * as hermanosSchema from "../schemas/hermanos";
+import * as mongoose from 'mongoose';
+import * as constantes from '../../../utils/constantes';
+import * as escuelaSchema from '../../tm/schemas/escuela';
+import * as autorizadosSchema from '../../tm/schemas/autorizados';
+import * as hermanosSchema from '../schemas/hermanos';
 
-export let alumnoSchema = new mongoose.Schema({
-  nombre: String,
-  apellido: String,
-  documento: String,
-  fechaNacimiento: Date,
-  direccion: { type: mongoose.SchemaTypes.Mixed },
-  localidad: String,
-  telefono: Number,
-  mail: String,
-  escuela: { type: escuelaSchema },
-  grado: constantes.GRADO,
-  libretaSanitaria: Boolean,
-  activo: Boolean,
-  tipoAlumno: constantes.TIPOALUMNO,
-  anoInicio: Number,
-  autorizados: [autorizadosSchema],
-  hermanos: [hermanosSchema],
-  observaciones: String,  
-  alumnoImage: {
-    size: String,
-    path: String,
-    filename: String,
-    destination: String,
-    mimetype: String,
-    encoding: String,
-    originalname: String,
-    fieldname: String
-  }
-}, { timestamps: { createdAt: 'created_at' } });
+export let alumnoSchema = new mongoose.Schema(
+	{		
+		nombre: String,
+		apellido: String,
+		documento: String,
+		fechaNacimiento: Date,
+		direccion: { type: mongoose.SchemaTypes.Mixed },
+		localidad: String,
+		telefono: Number,
+		mail: String,
+		escuela: { type: escuelaSchema },
+		grado: constantes.GRADO,
+		libretaSanitaria: Boolean,
+		activo: Boolean,
+		tipoAlumno: constantes.TIPOALUMNO,
+		anoInicio: Number,
+		autorizados: [ autorizadosSchema ],
+		hermanos: [ hermanosSchema ],
+		observaciones: String,
+		alumnoImage: {
+			size: String,
+			path: String,
+			filename: String,
+			destination: String,
+			mimetype: String,
+			encoding: String,
+			originalname: String,
+			fieldname: String
+		}
+	},
+	{ timestamps: { createdAt: 'created_at' } }
+);
 
-export let alumnos = mongoose.model("alumno", alumnoSchema, "alumnos");
+export let alumnos = mongoose.model('alumnos', alumnoSchema, 'alumnos');
